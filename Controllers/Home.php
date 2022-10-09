130acee90bd3541edc72da130a0a091fe7b641ba
Alan Lozano <alan.yo.99@gmail.com>
2022-10-09 07:16:29 -0300

    archivos añadidos

---
+++ Controllers/Home.php
@@ -0,0 +1,24 @@
+<?php 
+	require_once("Models/TCategoria.php");
+	require_once("Models/TProducto.php");
+	class Home extends Controllers{
+		use TCategoria, TProducto;
+		public function __construct()
+		{
+			parent::__construct();
+			session_start();
+		}
+
+		public function home()
+		{
+			$data['page_tag'] = "Tienda virtual PC Box";
+			$data['page_title'] = "Página principal";
+			$data['page_name'] = "home";
+			$data['slider'] = $this->getCategoriasT(CAT_SLIDER);
+			$data['banner'] = $this->getCategoriasT(CAT_BANNER);
+			$data['productos'] = $this->getProductosT();
+			$this->views->getView($this,"home",$data);
+		}
+
+	}
+ ?>
